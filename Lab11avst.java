b88e549c479124da5f89ffb6f2ad6480dce7feea
Dominickd1013 <domenico.damato@vtsd.com>
2023-01-06 22:30:04 -0500

    1/6/22

--- Lab11avst.java
+++ Lab11avst.java
@@ -1,7 +1,6 @@
 // Lab11avst.java
 // The "Sieve of Eratosthenes" Program
 // This is the student, starting version of the Lab11a assignment.
-
 import java.util.Scanner;
 
 public class Lab11avst {
@@ -9,39 +8,40 @@ public class Lab11avst {
         // This main method needs additions for the 100 point version.
         Scanner input = new Scanner(System.in);
         final int MAX = 100;
-        boolean primes[];
-        primes = new boolean[MAX];
-        computePrimes(primes);
-        displayPrimes(primes);
+        boolean isprime[];
+        isprime = new boolean[MAX];
+        computeisprime(isprime);
+        displayisprime(isprime);
     }
 
-    // Treat all numbers as prime, make a loop that does this and changes all values to true in computeprimes
-    // Step 2 remove primes
-    // To start remove multiples of 2
-    public static void computePrimes(boolean primes[]) {
+    public static void computeisprime(boolean isprime[]) {
 
         System.out.println("\nCOMPUTING PRIME NUMBERS");
         System.out.println("Values before the change.");
         System.out.print("\n");
 
-        for (int k = 1; k < primes.length; k ++) {
-            if (primes[k] == false) {
-                System.out.print(k + " ");
+        for (int p = 2; p < isprime.length; p++) {
+            isprime[p] = true;
+        }
+
+        System.out.print(isprime);
+        for (int i = 2; i < isprime.length; i++) {
+            for (int k = 2 * i; k < isprime.length; k += i) {
+                isprime[k] = false;
             }
         }
 
     }
 
-    public static void displayPrimes(boolean primes[]) {
-        System.out.println("\n\nPRIMES BETWEEN 1 AND " + primes.length);
+    public static void displayisprime(boolean isprime[]) {
+        System.out.println("\n\nPRIMES BETWEEN 1 AND " + isprime.length);
         System.out.println();
 
-        for (int k = 2 * 2; k < primes.length; k += 2) {
-            if (primes[k] == false) {
-                System.out.print(k + " ");
-            }
+        for (int k = 2; k < isprime.length; k++) {
+            if (isprime[k] == true) ;
+            System.out.print(k + " ");
         }
 
     }
-
 }
+
